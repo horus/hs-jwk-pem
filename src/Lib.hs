756c8c6b785803ca91006410eeb7d966e99f77ab
horus <horus.li@gmail.com>
2022-06-15 17:08:08 +0800

    Carry public key only

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,13 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Lib (encodePEM) where
 
-import Control.Monad (guard)
 import Crypto.Number.Serialize (os2ip)
-import Crypto.PubKey.RSA (PublicKey (PublicKey))
+import Crypto.PubKey.RSA qualified as RSA (PublicKey (PublicKey))
 import Data.ASN1.BinaryEncoding (DER (DER))
 import Data.ASN1.Encoding (encodeASN1')
 import Data.ASN1.Types (toASN1)
@@ -34,43 +31,25 @@ instance FromJSON N where
 newtype E = E Integer
 
 instance FromJSON E where
-  parseJSON = withText "E" $ \case
-    "AQAB" -> return $ E 65537
-    "AAEAAQ" -> return $ E 65537
-    e -> fail $ "unrecognized exponent " ++ show e
+  parseJSON = withText "E" $ \e ->
+    if e == "AQAB" || e == "AAEAAQ"
+      then return $ E 65537
+      else fail $ "unrecognized exponent " ++ show e
 
-data Jwk = Jwk
-  { kty :: Text,
-    n :: N,
-    e :: E
-    {--
-        kid :: Maybe Text,
-        alg :: Maybe Text,
-        use :: Maybe Text
-    --}
-  }
+newtype Jwk' = Jwk' {pubkey :: PubKey}
 
-instance A.FromJSON Jwk where
+instance A.FromJSON Jwk' where
   parseJSON = withObject "JWK" $ \o -> do
-    kty <- o .: "kty"
-    guard (kty == "RSA")
-    n <- o .: "n"
-    e <- o .: "e"
-    {--
-        kid <- o .:? "kid"
-        alg <- o .:? "alg"
-        use <- o .:? "use"
-    --}
-    return Jwk {..}
+    "RSA" :: Text <- o .: "kty"
+    N n <- o .: "n"
+    E e <- o .: "e"
+    return $ Jwk' $ PubKeyRSA $ RSA.PublicKey (size n 1) n e
 
-buildPEM :: Jwk -> L.ByteString
-buildPEM jwk = pemWriteLBS $ PEM "RSA PUBLIC KEY" [] $ asn1DER (rsaPubKey jwk.n jwk.e)
+buildPEM :: Jwk' -> L.ByteString
+buildPEM = pemWriteLBS . PEM "RSA PUBLIC KEY" [] . asn1DER . pubkey
 
 asn1DER :: PubKey -> ByteString
 asn1DER = encodeASN1' DER . flip toASN1 []
-
-rsaPubKey :: N -> E -> PubKey
-rsaPubKey (N n) (E e) = PubKeyRSA $ PublicKey (size n 1) n e
 
 size :: Integer -> Int -> Int
 size !n !i
@@ -78,4 +57,4 @@ size !n !i
   | otherwise = size n (i + 1)
 
 encodePEM :: L.ByteString -> Either String L.ByteString
-encodePEM = fmap buildPEM . A.eitherDecode @Jwk
+encodePEM = fmap buildPEM . A.eitherDecode @Jwk'
